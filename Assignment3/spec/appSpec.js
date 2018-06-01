e4d9699b15d0e16a8910d96d8f2b80508436c536
GURPAHUL-SINGH <gurpahulsingh13@gmail.com>
2018-06-01 14:07:59 -0500

    final

--- Assignment3/spec/appSpec.js
+++ Assignment3/spec/appSpec.js
@@ -1,12 +1,9 @@
-
-
-
 describe("App", () => {
   const clickButton = document.querySelector('#click-me')
   const counter = document.querySelector('#counter')
   describe("Counter", () => {
     it("is set to 120 when the button is clicked", () => {
-      
+
       clickButton.click();
       expect(counter.innerHTML).toEqual('120')
     })
@@ -14,38 +11,66 @@ describe("App", () => {
 
   describe("Button", () => {
     it("has an ID of 'click-me'", () => {
-     
+      expect(clickButton.id).toBe('click-me')
     })
   })
 
   describe("Factorial", () => {
     it("returns the factorial of positive numbers", () => {
       expect(factorial(7)).toBe(5040)
+
     })
 
     it("returns 1 if the provided number is less than 1 ", () => {
-
+      expect(factorial(-22)).toBe(1)
     })
   })
 
   describe("Computer", () => {
+    const newcomputer = new Computer();
     it("is created with a hard drive size of 512", () => {
-
+      expect(newcomputer.hardDriveSpace).toBe(512)
     })
 
     describe("installProgram", () => {
-      it("can install a program if there is sufficient space", () => {
+      const newcomputer = new Computer();
 
+      beforeEach(function (done) {
+        newcomputer.installProgram(400, function () {
+          done();
+        })
+      })
+      it("can install a program if there is sufficient space", (done) => {
+        expect(newcomputer.hardDriveSpace).toBe(112)
+        done()
+      })
+
+
+    })
+
+    describe("installProgram", function () {
+      const newcomputer = new Computer();
+
+      beforeEach(function (done) {
+        newcomputer.installProgram(700, function () {
+          done();
+        })
       })
 
       it("will not install the program if there is insufficient space", () => {
 
+        expect(newcomputer.hardDriveSpace).toBe(512)
       })
     })
 
+
     describe("format", () => {
+      const newcomputer = new Computer();
       it("resets the hard drive to 512, even after programs have been installed", () => {
 
+        newcomputer.hardDriveSpace = 400
+        newcomputer.format();
+        expect(newcomputer.hardDriveSpace).toBe(512)
       });
     })
 
